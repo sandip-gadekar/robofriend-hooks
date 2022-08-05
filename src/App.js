d821d4185d7d719aebd83fa5b24b77eecece4059
sandip-gadekar <57319706+sandip-gadekar@users.noreply.github.com>
2022-08-06 00:47:44 +0530

    updated class componet into hooks

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from './CardList';
 import Search from './Search';
 import './App.css'
 import Scroll from './Scroll';
 
-export class App extends Component {
-  constructor() {
-    super(); //calls to component class constructor
-    this.state = {
-      robots: [],
-      searchField: ''
-    }
+function App() {
+  // constructor() {
+  //   super(); //calls to component class constructor
+  //   this.state = {
+  //     robots: [],
+  //     searchField: ''
+  //   }
+  const [robots, setRobots] = useState([])
+  const [searchField, setSearchField] = useState('');
 
-    console.log("Constuctor");//1st class constructor grt called
+  // componentDidMount() {
+  //   fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(users => this.setState({ robots: users }));
+  //   console.log("didmount method get called");// class constructor grt called
+  // }//by using this object get created then(response=>response.json())
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => setRobots(users));
+  })
+  const onsearch = (event) => {
+    setSearchField(event.target.value)
   }
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(users => this.setState({ robots: users }));
-    console.log("didmount method get called");// class constructor grt called
-  }//by using this object get created then(response=>response.json())
-  onsearch = (event) => {
-    //console.log(event.target.value);
-    this.setState({ searchField: event.target.value })
-  }
-  render() {
-    console.log("render method get called ");
-    const filteredRobots = this.state.robots.filter(robots => {//searching logic and pasing filteredrobots variable as props tocardlist
-      return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
-    })
-
-    //console.log(filteredRobots);
-    return (
-      <div>
-        <h1 className='tc'>ROBO FRIENDSs</h1>
-        <Search onsearch={this.onsearch} />
-        <Scroll>
-          <CardList robots={filteredRobots} />
-        </Scroll>
-      </div>
-    )
-  }
+  //console.log(searchField);
+  const filteredRobots = robots.filter(robots => {//searching logic and pasing filteredrobots variable as props to cardlist
+    return robots.name.toLowerCase().includes(searchField.toLowerCase())
+  })
+  return (
+    <div>
+      <h1 className='tc'>ROBO FRIENDS</h1>
+      <Search onsearch={onsearch} />
+      <Scroll>
+        <CardList robots={filteredRobots} />
+      </Scroll>
+    </div>
+  )
 }
+
 
 export default App
